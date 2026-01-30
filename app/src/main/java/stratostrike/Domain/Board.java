6f31d9827f51fa91a76178ab14a1fbd8e23abd52
federicodeca <federicodeca98@gmail.com>
2026-01-30 17:18:53 +0100

    setJ M

--- app/src/main/java/stratostrike/Domain/Board.java
+++ app/src/main/java/stratostrike/Domain/Board.java
@@ -1,18 +1,21 @@
 package stratostrike.Domain;
-
+import java.util.List;
 public class Board {
     private final int width;
     private final int length;
+    
     private final int levels;
+    private final List<String> levelNames; // Dinamico!
     private final Position[][][] positions;
 
-    public Board(int width, int length, int levels) {
+    public Board(int width, int length, List<String> names) {
+
         this.width = width;
         this.length = length;
-        this.levels = levels;
+        this.levelNames = names;
+        this.levels = names.size(); 
         this.positions = new Position[width][length][levels];
-
-        for (int x = 0; x < width; x++) {
+               for (int x = 0; x < width; x++) {
             for (int y = 0; y < length; y++) {
                 for (int z = 0; z < levels; z++) {
                     positions[x][y][z] = new Position(x, y, z);
@@ -20,6 +23,11 @@ public class Board {
             }
         }
     }
+
+    public String getLevelName(int z) {
+        return levelNames.get(z);
+    }
+
 
     public Position getPosition(int x, int y, int z) {
         return positions[x][y][z];
--- app/src/main/java/stratostrike/Settings.java
+++ app/src/main/java/stratostrike/Settings.java
@@ -1,9 +1,12 @@
 package stratostrike;
-
+import java.util.List;
 public class Settings {
 
     // Settings for the Stratostrike game: board dimensions
     public static final int BoardLengthStandard = 20;
     public static final int BoardWidthStandard = 20;
-    public static final int BoardLevelsStandard = 2;
+    public static final List<String> BoardLevelsStandard = List.of(new String[] {
+        "CIELO",
+        "SPAZIO"
+    });
 }
